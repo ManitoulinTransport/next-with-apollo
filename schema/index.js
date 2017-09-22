aa4f338844a2fac2b510452a5263978df8b8fe3f
Matthew Francis Brunetti <mbrunetti@manitoulintransport.com>
2017-09-22 12:22:29 -0400

    Move schema folder to top level

---
+++ schema/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+const {makeExecutableSchema} = require('graphql-tools')
+const resolvers = require('./resolvers')
+
+const typeDefs = fs.readFileSync(path.join(__dirname, 'typeDefs.graphql'), 'utf8')
+
+module.exports = makeExecutableSchema({ typeDefs, resolvers })
--- server/index.js
+++ server/index.js
@@ -3,19 +3,18 @@ const next = require('next')
 const {graphqlExpress, graphiqlExpress} = require('graphql-server-express')
 const bodyParser = require('body-parser')
 const pCall = require('../util/pCall')
-const getSchema = require('./schema')
+const schema = require('../schema')
 
 main()
 
 async function main() {
     const port = parseInt(process.env.PORT, 10) || 3000
     const dev = process.env.NODE_ENV !== 'production'
-    const app = next({dev})
 
-    const [, schema] = await Promise.all([app.prepare(), getSchema()])
+    const app = next({dev})
+    await app.prepare()
 
     const server = express()
-
     server.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
     server.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}))
     server.get('*', app.getRequestHandler())
