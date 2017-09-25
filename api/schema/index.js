ded5613532c1f850f43190deba69d809e53ceff8
Matthew Francis Brunetti <mbrunetti@manitoulintransport.com>
2017-09-25 12:34:46 -0400

    Store typeDefs in plaintext

--- api/schema/index.js
+++ api/schema/index.js
@@ -1,5 +1,8 @@
+const fs = require('fs')
+const path = require('path')
 const { makeExecutableSchema } = require('graphql-tools')
-const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers')
+
+const typeDefs = fs.readFileSync(path.join(__dirname, 'typeDefs.graphql'), 'utf8')
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers })
