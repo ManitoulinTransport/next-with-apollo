cb56a85b007ab1d1ef7ee97aca9bfd1e9a2bd2f5
Matthew Francis Brunetti <mbrunetti@manitoulintransport.com>
2017-09-26 07:35:13 -0400

    Formatting

--- api/schema/index.js
+++ api/schema/index.js
@@ -3,6 +3,9 @@ const path = require('path')
 const { makeExecutableSchema } = require('graphql-tools')
 const resolvers = require('./resolvers')
 
-const typeDefs = fs.readFileSync(path.join(__dirname, 'typeDefs.graphql'), 'utf8')
+const typeDefs = fs.readFileSync(
+  path.join(__dirname, 'typeDefs.graphql'),
+  'utf8'
+)
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers })
